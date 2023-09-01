afd041393a6723479a341590c6fe35566562878e
Lino Jorge <109708821+Linolonil@users.noreply.github.com>
2023-09-01 10:40:28 -0400

    banco de dados na nuvem e porta atualizada

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb://127.0.0.1:27017";
+// const url = "mongodb://127.0.0.1:27017";
+const url = "mongodb+srv://admin:n2mHsvk07wkvcqt7@cluster0.gzavpjb.mongodb.net/";
 // const url = "mongodb+srv://admin:V90K7ehx2krw7OlM@cluster0.gbnr4oi.mongodb.net";
 const dbName = "jornada-backend-agosto-23";
 const client = new MongoClient(url);
@@ -98,7 +99,7 @@ const main = async () => {
     res.status(204).send();
   });
 
-  app.listen(3000);
+  app.listen(process.env.PORT || 3000);
 };
 
 main();
